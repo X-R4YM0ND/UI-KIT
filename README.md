3a4e8015ce32345aa50cabdf14b9e25df1dd68d6
Zhy <rayke4i@outlook.com>
2026-03-15 17:21:41 -0500

    chore: initial release of public ctrl-ui-kit with sanitized assets

---
+++ README.md
@@ -0,0 +1,8 @@
+# CTRL UI KIT 🚀
+A specialized UI Library built with **Angular 20**, **Signals**, and **Ionic**.
+
+## Features
+- ⚡️ High performance with Angular Signals.
+- 🎨 Centralized Design System (Inter & JetBrains Mono).
+- 🌙 Native Dark/Light mode support.
+- 🏗 Standalone Components.
--- src/public-api.ts
+++ src/public-api.ts
@@ -4,6 +4,5 @@
 
 export * from './lib/atoms/ctrl-button/ctrl-button.component';
 export * from './lib/atoms/ctrl-input/ctrl-input.component';
-export * from './lib/atoms/ctrl-icon/ctrl-icon.component';
 export * from './lib/atoms/ctrl-divider/ctrl-divider';
 export * from './lib/atoms/ctrl-radiobutton/ctrl-radiobutton';
